d1bd21c41b3b5c9b954d982b11e4b0a9df6d31a6
Kaustav Banerjee <kaustavsmailbox21@gmail.com>
2017-09-12 11:08:17 +0000

    Update step_1.sh

--- step_1.sh
+++ step_1.sh
@@ -1,1 +1,15 @@
-sudo apt-get update;sudo apt-get upgrade;sudo apt-get install python-pip git wget;sudo pip install tensorflow;sudo apt-get install protobuf-compiler python-pil python-lxml;sudo pip install jupyter;sudo pip install matplotlib;sudo git clone https://www.github.com/kaustav1996/object_detection;cd object_detection;sudo mkdir data;sudo git clone https://github.com/tensorflow/models;cd models;sudo protoc object_detection/protos/*.proto --python_out=.;export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim;sudo python object_detection/builders/model_builder_test.py;sudo python setup.py install;cd ..;
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python-pip git wget
+sudo pip install tensorflow
+sudo apt-get install protobuf-compiler python-pil python-lxml
+sudo pip install jupyter
+sudo pip install matplotlib
+sudo mkdir data
+sudo git clone https://github.com/tensorflow/models
+cd models
+sudo protoc object_detection/protos/*.proto --python_out=.
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+sudo python object_detection/builders/model_builder_test.py
+sudo python setup.py install
+cd ..
