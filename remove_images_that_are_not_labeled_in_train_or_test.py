ad4fbb09363decbeb638c69e47ae7dd92fef950c
Kaustav Banerjee <kaustavsmailbox21@gmail.com>
2017-10-16 18:39:11 +0530

    Create remove_images_that_are_not_labeled_in_train_or_test.py

---
+++ remove_images_that_are_not_labeled_in_train_or_test.py
@@ -0,0 +1,8 @@
+from os import listdir
+from os.path import isfile, join
+from os import system
+onlyfiles = [f.split(".")[0] for f in listdir("./") if isfile(join("./", f))]
+no_repeat = list(set(onlyfiles))
+for x in no_repeat:
+	if onlyfiles.count(x)==1 and x!='file':
+		system("rm "+x+".*")
